4139c8e4afd569bace30f5eca6eea7916b0dacd5
Jeff Mekler <jmekler@gmail.com>
2014-04-06 09:52:14 -0400

    Seeding data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,20 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-u1 = User.create(name: "Josh Chou", username: "joshthedudeoflife")
-u2 = User.create(name: "Jeff Mekler", username: "jmekler")
+require "CSV"
 
-c = Challenge.create(title: "Electricity challenge", description: "Reduce your energy usege at home.", start_date: "1/1/14", end_date: "4/1/14")
-c.steps.create(title: "Replace a light bulb.", description: "Be cool yo.", pledge_description: "replaced old light bulbs.", pledge_points: 10);
-c.steps.create(title: "Replace windows.", description: "Be cool yo.", pledge_description: "upgraded windows.", pledge_points: 100);
+# u1 = User.create(name: "Josh Chou", username: "joshthedudeoflife")
+# u2 = User.create(name: "Jeff Mekler", username: "jmekler")
+# 
+# c = Challenge.create(title: "Electricity challenge", description: "Reduce your energy usege at home.", start_date: "1/1/14", end_date: "4/1/14")
+# c.steps.create(title: "Replace a light bulb.", description: "Be cool yo.", pledge_description: "replaced old light bulbs.", pledge_points: 10);
+# c.steps.create(title: "Replace windows.", description: "Be cool yo.", pledge_description: "upgraded windows.", pledge_points: 100);
+# 
+# u1.memberships.create(challenge: c)
+# u2.memberships.create(challenge: c)
 
-u1.memberships.create(challenge: c)
-u2.memberships.create(challenge: c)
+
+energy_challenge = Challenge.create(:)
+CSV.foreach("db/seeds/energy_steps.csv") do |row|
+  energy_challenge.steps.create(
+end
